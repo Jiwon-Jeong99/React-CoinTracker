bee616e100e50bed0d3afb626abe2a87373e03ab
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-08-09 20:30:47 +0900

    스타일 컴포넌트 기본 이용

--- master/src/App.js
+++ master/src/App.js
@@ -15,11 +15,28 @@ const Circle = styled(Box)`
   border-radius: 50px;
 `;
 
+const Btn = styled(Box)`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
+`
+
+// attrs로 Input 컴포넌트 전체에 required:true 속성을 먹일 수 있음
+const Input = styled.input.attrs({required: true})`
+  color: tomato;
+`
+
 function App() {
   return (
-    <Father>
+    // Father 컴포넌트를 header로 사용
+    <Father as="header">
       <Box bgColor="teal" />
       <Circle bgColor="tomato" />
+      <Btn>Log in</Btn>
+      {/* btn을 a태그로 사용하고 싶을 때 */}
+      <Btn as="a" href="/">Log in</Btn>
+      <Input />
     </Father>
   );
 }
