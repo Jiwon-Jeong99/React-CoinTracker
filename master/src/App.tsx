e578fe9bd311bcfd8c5a3d3b4a1cace9c6e599e5
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-08-10 01:03:24 +0900

    typescript props

---
+++ master/src/App.tsx
@@ -0,0 +1,12 @@
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <>
+      <Circle borderColor="yellow" bgColor="teal" />
+      <Circle text="im here" bgColor="tomato" />
+    </>
+  );
+}
+
+export default App;
---
+++ master/src/Circle.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+interface ContainerProps {
+  bgColor: string;
+  //   여기서는 borderColor가 required-default
+  borderColor: string;
+}
+
+// ContainerProps를 상속받은 Container 컴포넌트의 속성
+const Container = styled.div<CircleProps>`
+  width: 200px;
+  height: 200px;
+  background-color: ${(props) => props.bgColor};
+  border-radius: 100px;
+  /* 왜냐하면 Container 속성에 borderColor가 있기 때문에 borderColor는 default로 필요 */
+  border: 1px solid ${(props) => props.borderColor};
+`;
+
+// CircleProps의 bgColor는 string 형태일 것이다.
+interface CircleProps {
+  bgColor: string;
+  borderColor?: string;
+  text?: string;
+}
+
+// bgColor의 형태는 CircleProps를 기반으로 해야 한다
+const Circle = ({
+  bgColor,
+  borderColor,
+  text = "default text",
+}: CircleProps) => {
+  return (
+    <>
+      {/* borderColor에 아무것도 적지 않으면 기본값으로 white를 주겠다 */}
+      <Container bgColor={bgColor} borderColor={borderColor ?? "white"}>
+        {text}
+      </Container>
+    </>
+  );
+};
+
+export default Circle;
