33e285aba201e672b9ebfc1dc7e7256844592e6a
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-08-13 00:18:29 +0900

    theme

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,10 +1,19 @@
+import React, { useState } from "react";
+import styled from "styled-components";
 
+// theme 내부로 접근 가능(props도)
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
   return (
-    <>
-    
-    </>
+    <Container>
+      <H1>protected</H1>
+    </Container>
   );
 }
 
---
+++ typescript/src/styled.d.ts
@@ -0,0 +1,10 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    // 내 테마가 어떻게 보일지 설명하는 부분
+    textColor: string;
+    bgColor: string;
+    btnColor: string;
+  }
+}
